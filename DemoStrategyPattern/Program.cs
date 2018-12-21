65a9d548f6657bdad45cc8afc7ccb4e5c5623ea1
Hai nam LE TRINH <hai-nam.letrinh@bollore.com>
2018-12-21 13:06:16 +0700

    first commit

---
+++ DemoStrategyPattern/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoStrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Declaration
+            var strategies = new Dictionary<EnumPerson, IPerson>
+            {
+                {EnumPerson.Asian, new AsianPerson()},
+                { EnumPerson.Europe, new EuropePerson()}
+            };
+
+            //Action
+
+            var param = new ParamModel
+            {
+                Name = "Nam",
+                Description = "Nam Desc"
+            };
+
+            strategies[EnumPerson.Europe].DoWork(param);
+            //strategies[EnumPerson.Asian].DoWork(param);
+
+            Console.ReadLine();
+        }
+    }
+
+    enum EnumPerson
+    {
+        Asian,
+        Europe
+    }
+
+    class ParamModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    interface IPerson
+    {
+        void DoWork(ParamModel model);
+    }
+
+    class AsianPerson : IPerson
+    {
+        public void DoWork(ParamModel model)
+        {
+            Console.WriteLine("Asian Person do work - " + model.Name);
+        }
+    }
+
+    class EuropePerson : IPerson
+    {
+        public void DoWork(ParamModel model)
+        {
+            Console.WriteLine("Europe Person do work - " + model.Name);
+        }
+    }
+}
